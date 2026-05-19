71909d4ece79074a7ef2b5c401c8f3d10d73b519
joelchenko <joelchenko@gmail.com>
2026-05-19 18:29:36 -0300

    a mi me toca el año 2010

---
+++ joel_2010.R
@@ -0,0 +1,13 @@
+rm(list = ls())
+
+library(tidyverse)
+
+load("full_intendentes_margen_2001_2021.RData")
+
+intent_2010 <- full_intendentes_margen_2001_2021 %>% 
+  filter(ano == 2010)
+
+# que porcentaje de los intendentes son de sexo femenino
+# cual es la edad promedio de los intendentes
+# de los intendentes colorados, cual es el promedio de edad
+# cual es el departamento con mayor porcentaje de intendentes NO-COLORADOS
