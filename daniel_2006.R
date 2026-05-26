13838ec4231398245a4497976f18fc3f459196ca
MacBook air <dbenitez@desarrollo.edu.py>
2026-05-26 18:14:26 -0300

    Actualizacion

--- daniel_2006.R
+++ daniel_2006.R
@@ -11,5 +11,14 @@ inte_2006 <- full_intendentes_margen_2001_2021 %>% filter(ano == 2006)
 # cual es la edad promedio de los intendentes
 # de los intendentes colorados, cual es el promedio de edad
 # cual es el departamento con mayor porcentaje de intendentes NO-COLORADOS
+# cuantas intendentes mujeres hay en cada departamento
 
+inte_2006 %>% summarise(porcentaje_femenino = sum (sexo == "F", na.rm = TRUE)/ sum(!is.na(sexo)) * 100)
 
+inte_2006 %>% summarise(edad_promedio = mean(edad, na.rm = TRUE))
+
+inte_2006 %>% filter(siglas_lista == "ANR") %>% summarise(edad_promedio_colorados = mean(edad, na.rm = TRUE))
+
+inte_2006 %>% group_by(depdes) %>% summarise(porcentaje_no_colorados = sum(siglas_lista != "ANR", na.rm = TRUE)/ sum(!is.na(siglas_lista)) * 100) %>% arrange(desc(porcentaje_no_colorados))
+
+inte_2006 %>% group_by(depdes) %>% summarise(cant_mujeres = sum(sexo == "F", na.rm = TRUE)) %>% arrange(desc(cant_mujeres))
